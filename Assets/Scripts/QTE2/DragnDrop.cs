a51b4671b367c80d35f780db881b0ceb5949d351
Naxa <ignaciaruizperez1@gmail.com>
2024-10-12 21:47:52 -0300

    Minijuego2 terminado

--- Assets/Scripts/QTE2/DragnDrop.cs
+++ Assets/Scripts/QTE2/DragnDrop.cs
@@ -8,6 +8,11 @@ public class DragnDrop : MonoBehaviour
     private bool isDragging = false;
     private Vector3 offset;
 
+    [Range(0f, 1f)]
+    public float igniteProbability = 0.5f; 
+
+    bool ignited = false;
+
     void OnMouseDown()
     {
         isDragging = true;
@@ -19,7 +24,7 @@ public class DragnDrop : MonoBehaviour
         if (isDragging)
         {
             Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
-            newPosition.z = 0; 
+            newPosition.z = 0;
             transform.position = newPosition;
         }
     }
@@ -31,9 +36,34 @@ public class DragnDrop : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("FosforoZona"))
+        if (collision.gameObject.CompareTag("FosforoZona") && !ignited)
         {
-            Debug.Log("Colision");
+            TryIgnite();
         }
+
+        if (collision.gameObject.CompareTag("ConopaZona") && ignited)
+        {
+            Debug.Log("Prendiste la wea");
+        }
+    }
+
+    private void TryIgnite()
+    {
+        float randomValue = Random.Range(0f, 1f);
+        if (randomValue <= igniteProbability)
+        {
+            ChangeColor(Color.blue); 
+            Debug.Log("¡El fósforo se encendió!");
+            ignited = true;
+        }
+        else
+        {
+            Debug.Log("El fósforo no se encendió.");
+        }
+    }
+
+    private void ChangeColor(Color newColor)
+    {
+        GetComponent<Renderer>().material.color = newColor;
     }
 }
